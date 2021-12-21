a40b9703109b77fbfcb78aa7e767c1737425b0ab
Diane Tritchler <dtritchler92@gmail.com>
2021-12-20 21:38:36 -0600

    Added Validation

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: gitHubName => {
+                if(gitHubName){
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -38,6 +54,7 @@ const promptProject = portfolioData => {
     if (!portfolioData.projects) {
         portfolioData.projects = [];
     }
+   
 
     console.log(`
     =================
@@ -49,12 +66,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: projectName => {
+            if(projectName){
+                return true;
+            } else {
+                console.log('Please enter your Project Name!');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescription => {
+            if(projectCescription){
+                return true;
+            } else {
+                console.log('Please enter a Project Description!');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -82,10 +115,19 @@ const promptProject = portfolioData => {
     ])
     .then(projectData => {
         portfolioData.projects.push(projectData);
-    });
+    })
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
   };
 
   promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
